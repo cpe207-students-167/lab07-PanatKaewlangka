6f637c864a2a8f4535c315ecc3fac43771b97b75
PanatKaewlangka <panut4207315@gmail.com>
2024-07-16 12:53:53 +0700

    done

--- readme.md
+++ readme.md
@@ -3,11 +3,11 @@
 
 ### ป้อนข้อมูลนักศึกษา
 
-รหัส นศ.:
+รหัส นศ.:660610776
 
-ชื่อ-สกุล :
+ชื่อ-สกุล :ปาณัท แก้วลังกา
 
-GitHub Page URL :
+GitHub Page URL : https://cpe207-students-167.github.io/lab07-PanatKaewlangka/
 
 ---
 
--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,13 @@
 // create reference for input fields.
 const firstNameInput = document.querySelector("#first-name-input");
 const lastNameInput = document.querySelector("#last-name-input");
+const email = document.querySelector("#email-input");
+const password = document.querySelector("#password-input");
+const confirmPassword = document.querySelector("#password-confirm-input");
 
 // create reference for buttons.
 const submitBtn = document.querySelector("#submit-btn");
+const resetBtn = document.querySelector("#reset-btn");
 
 // simple email validation
 function validateEmail(email) {
@@ -20,13 +24,36 @@ firstNameInput.onkeyup = () => {
 
 // add callback functions for other input events.
 // (lastname, email, password, confirm password)
+lastNameInput.onkeyup = () => {
+  lastNameInput.classList.remove("is-valid");
+  lastNameInput.classList.remove("is-invalid");
+};
+
+email.onkeyup = () => {
+  email.classList.remove("is-valid");
+  email.classList.remove("is-invalid");
+};
+
+password.onkeyup = () => {
+  password.classList.remove("is-valid");
+  password.classList.remove("is-invalid");
+};
+
+confirmPassword.onkeyup = () => {
+  confirmPassword.classList.remove("is-valid");
+  confirmPassword.classList.remove("is-invalid");
+};
 
 // add callback function for submit button.
 submitBtn.onclick = () => {
-  isFirstNameOk = false;
+  let isFirstNameOk = false;
+  let isLastNameOk = false;
+  let isEmailOk = false;
+  let isPasswordOk = false;
+  let isConfirmPasswordOk = false;
 
   // validate first name
-  if (firstNameInput.value !== "CPE207") {
+  if (firstNameInput.value === '') {
     firstNameInput.classList.add("is-invalid");
   } else {
     firstNameInput.classList.add("is-valid");
@@ -34,14 +61,50 @@ submitBtn.onclick = () => {
   }
 
   // validate last name
-
+  if (lastNameInput.value.trim() === '') {
+    lastNameInput.classList.add("is-invalid");
+  } else {
+    lastNameInput.classList.add("is-valid");
+    isLastNameOk = true;
+  }
   // validate email
-
+  if (validateEmail(email.value)) {
+    email.classList.add("is-valid");
+    isEmailOk = true;
+  } else {
+    email.classList.add("is-invalid");
+    isEmailOk = false;
+  }
   // validate password
-
+  if (password.value === '') {
+    password.classList.add("is-invalid");
+  } else if(password.value.length < 6){
+    password.classList.add("is-invalid");
+  } else {
+    password.classList.add("is-valid");
+    isPasswordOk = true;
+  }
   // validate confirm password
+  if (confirmPassword.value === '') {
+    confirmPassword.classList.add("is-invalid");
+  } else if(confirmPassword.value!== password.value){
+    confirmPassword.classList.add("is-invalid");
+  } else {
+    confirmPassword.classList.add("is-valid");
+    isConfirmPasswordOk = true;
+  }
 
-  if (isFirstNameOk) alert("Registered successfully");
+  if (isFirstNameOk && isLastNameOk && isEmailOk && isPasswordOk && isConfirmPasswordOk) alert("Registered successfully");
 };
 
 // add callback function for Reset button.
+let form = document.querySelector("#regform");
+let inputs = [firstNameInput, lastNameInput, email, password, confirmPassword];
+resetBtn.onclick = () => {
+  form.reset();
+  inputs.forEach(input => {
+    input.value = ''; 
+    input.classList.remove("is-valid"); 
+    input.classList.remove("is-invalid"); 
+  });
+};
